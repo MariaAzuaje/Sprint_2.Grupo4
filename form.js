fc3351f415ed49548a9d36e54b836d7723f3ee9a
MariaAzuaje <mazuajeporras@gmail.com>
2021-09-22 11:10:06 -0500

    documento actualiado sprint 2

---
+++ form.js
@@ -0,0 +1,26 @@
+"use strict"
+
+function checkNombre(valor){
+    let exreNombre = /^[a-zA-Z\s]{4,30}+$/;
+}
+module.exports = checkNombre;
+
+function checkApellido(valor){
+    let exreApellido 
+}
+module.exports = checkApellido;
+
+function checkTelefono(valor){
+    let exreTelefono
+}
+module.exports = checkTelefono;
+
+function checkCorreo(valor){
+    let exreCorreo
+}
+module.exports = checkCorreo;
+
+function checkContrasena(valor){
+    let exreContrasena
+}
+module.exports = checkContrasena;
